68982cabeacde19f9d9a65df19b7c2ec2f605a9a
carlos-ahr <cherrera2100@alumno.ipn.mx>
2023-04-02 14:57:32 -0600

    02-04-2023

---
+++ Testing/include/nodo.cpp
@@ -0,0 +1,13 @@
+#include "nodo.h"
+
+Nodo::Nodo (int v, Nodo* s){
+    dato = v;
+    siguiente = s;
+}
+int Nodo::obtenerValor(void){return dato;}
+Nodo* Nodo::obtenerSiguiente(void){return siguiente;}
+void Nodo::cambiarValor(int v){dato = v;}
+void Nodo::cambiarSiguiente(Nodo* s){siguiente = s;}
+Nodo::~Nodo(){
+
+}
--- Testing/include/pausa.cpp
+++
@@ -1,9 +0,0 @@
-#include "pausa.h"
-#include <iostream>
-
-namespace pausa{
-    void pausa(void) {
-        std::cout<<"Presione cualquier tecla para continuar...";
-        std::cin.get();
-    }
-}
