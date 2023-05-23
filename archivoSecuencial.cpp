7f0f1cac05ca9b155ab19af6f587b010135fa304
carlos-ahr <89262680+carlos-ahr@users.noreply.github.com>
2023-05-23 03:27:31 +0000

    22-05-23

--- archivoSecuencial.cpp
+++ archivoSecuencial.cpp
@@ -1,28 +1,27 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(void) {
-    //Variable de tipo caracter para manejo del bucle
+    // Variable de tipo caracter para manejo del bucle
     char res;
-    //Abrir archivo en modo escritura:
+    // Abrir archivo en modo escritura:
     ofstream archivo("archivo.txt");
-    //Variable de tipo cadena de caracteres utilizadacomo buffer
+    // Variable de tipo cadena de caracteres utilizadacomo buffer
     string buff;
-    do
-    {
-        //Limpiando pantalla
+    do {
+        // Limpiando pantalla
         system("clear");
         cin.get();
-        //Pidiendo informacion a agregar:
+        // Pidiendo informacion a agregar:
         cout << "Ingrese lo que desea agregar al archivo: ";
         getline(cin, buff);
-        //Escribiendo informacion en el archivo:
+        // Escribiendo informacion en el archivo:
         archivo << buff << endl;
-        //Reiniciando bucle:
+        // Reiniciando bucle:
         cout << "\nDesea ejecutar el programa de nuevo? (s/n): ";
         cin >> res;
         res = tolower(res);
